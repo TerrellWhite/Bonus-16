1a2443a97b4a1a008e6fa5dc8e2fe7731deadf3f
TerrellWhite <Terrell.White053100@gmail.com>
2018-07-31 09:05:17 -0400

    First go

---
+++ GC_Bonus-16/Car.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace GC_Bonus_16
+{
+    class Car
+    {
+        #region Fields
+        private string make;
+        private string model;
+        private int year;
+        private double price;
+        #endregion
+
+        #region Properties
+        public string Make
+        {
+            set { make = value; }
+            get { return make; }
+        }
+        public string Model
+        {
+            set { model = value; }
+            get { return model; }
+        }
+        public int Year
+        {
+            set
+            {
+                if (!Regex.IsMatch(Convert.ToString(value), @"^\d{4}$"))
+                {
+                    throw new Exception("years is not vaild!");
+                }
+                else
+                {
+                    year = value;
+                }
+                year = value;
+            }
+            get { return year; }
+        }
+        public double Price
+        {
+            set
+            {
+                if (!Regex.IsMatch(Convert.ToString(value), @"^\d{4,}$"))
+                {
+                    throw new Exception("Price is not vaild!");
+                }
+                else
+                {
+                    price = value;
+                }
+                price = value;
+            }
+            get { return price; }
+        }
+        #endregion
+    }
+}
---
+++ GC_Bonus-16/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GC_Bonus_16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // List of Employees
+            List<Car> cars = new List<Car>();
+
+            //used to add new employees to the list
+            while (true)
+            {
+                Car temp = new Car();
+                Console.WriteLine("Please Provide the Make:");
+                temp.Make = Console.ReadLine();
+                Console.WriteLine("Please Provide the Model:");
+                temp.Model = Console.ReadLine();
+                Console.WriteLine("Please Provide the Year:");
+                temp.Year = int.Parse(Console.ReadLine());
+                Console.WriteLine("Please Provide the Price:");
+                temp.Price = double.Parse(Console.ReadLine());
+
+                cars.Add(temp); //add the temp object to the employees list
+                Console.WriteLine("Do you want to add more cars?");
+                string choice = Console.ReadLine();
+                if (choice.ToLower() == "n")
+                {
+                    break;
+                }       
+            }
+            foreach (Car e in cars)
+            {
+                Console.WriteLine($"{e.Make} {e.Model} \n${e.Price} {e.Year}");
+                Console.WriteLine("=====================");
+
+            }
+        }
+    }
+}
